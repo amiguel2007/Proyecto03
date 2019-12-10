970ae958d180766d405a4ffbd82f1000aab0df8e
ANtonio Miguel <amiguel2007@gmail.com>
2019-12-10 20:02:35 +0100

    Mejora

--- src/Proyecto01/Ejemplo01.java
+++ src/Proyecto01/Ejemplo01.java
@@ -1,6 +1,8 @@
 package Proyecto01;
 import java.util.Scanner;
 
+// Creado por Antonio Miguel
+
 public class Ejemplo01 {
     public static void main(String[] args) {
         Scanner obtenerNumero = new Scanner(System.in);
@@ -9,7 +11,7 @@ public class Ejemplo01 {
         System.out.print("indica la tabla de multiplicar: ");
         numero = obtenerNumero.nextInt();
  
-        for(i = 1; i<=numero; i++)
+        for(i = 1; i <= numero; i++)
         {
             for(j = 1; j <= 12; j++)
             {
